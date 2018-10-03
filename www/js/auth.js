4a7b1c2e0f89fad3d7001c7db636e56ff3101197
Tobi Okedeji <Tobi Okedeji>
2018-10-03 23:07:28 +0100

    adding UI

--- www/js/auth.js
+++ www/js/auth.js
@@ -1,5 +1,177 @@
 // =======================Log the Driver into the app after signup from web====================================
 function login(){
+    $("#login").attr("onClick", "doNothing()");
+    $.mobile.loading("show");
+
+    authDriver();
+}
+
+function authDriver(){
+    $.ajax({
+        type: "post",
+        url: "http://teraraveweb.herokuapp.com/mobile/login",
+        data: {
+            email: $("#email").val(),
+            password: $("#password").val(),
+        },
+        success: function(data) {
+            if (data.status == true) {
+                console.log(data.message)
+
+                $.mobile.loading("hide");
+                $("#error").empty()
+                $("#success").html(data.message)
+                localStorage.setItem("token", data.token);
+                localStorage.setItem("fullname", data.fullname);
+                localStorage.setItem("id", data.id);
+                $.mobile.navigate("#main-page");
+
+            } else if (data.status == false) {
+                console.log(data.message)
+
+                $.mobile.loading("hide");
+                $("#error").html(data.message)
+                $("#login").attr("onClick", "login()")
+
+            }
+        },
+        error: function(error){
+            $.mobile.loading("hide");
+            $("#error").html("Error while Logging in")
+            $("#login").attr("onClick", "login()")
+        }
+    })
+}
+
+//================================Check if Logged in and direct to main page/login page =============================
+$(document).on("pagecontainerbeforechange", function (event, ui) {
+    if (typeof ui.toPage !== "object") return;
+    switch (ui.toPage.attr("id")) {
+        case "login-page":
+            var token = localStorage.getItem("token")
+
+            if (!ui.prevPage && token) {            // Check for token and redirect to main page.
+                ui.toPage = $("#main-page");  
+
+                $.extend( ui.options, {
+                    transition: "pop",
+                });
+            }else if(ui.prevPage && !token){        // Check if logged out already
+                ui.toPage = $("#login-page");  
+
+                $.extend( ui.options, {
+                    transition: "slidefade",
+                });
+            }else if(!ui.prevPage && !token){        // Check if first time to open the app
+                ui.toPage = $("#login-page");  
+
+                $.extend( ui.options, {
+                    transition: "pop",
+                });
+            }else if(ui.prevPage && token){        // Check if 
+                ui.toPage = $("#main-page");  
+
+                $.extend( ui.options, {
+                    transition: "slidefade",
+                });
+            }
+    }
+
+});
+
+$(document).on("pagecontainerbeforeshow", function (event, ui) {
+    if (typeof ui.toPage == "object") {
+        var fullname = localStorage.getItem("fullname"),
+            id = localStorage.getItem("id");
+
+        switch (ui.toPage.prop("id")) {
+            case "main-page":
+                $("#fullname").text(fullname);
+                $("#id").text(id);
+                break;
+            case "track-page":
+                $("#driveId").text(id);
+                break;
+            default:
+                break;
+        }
+    }
+});
+
+//handle the back button and then populate the parameters here with what is in the database. to be done before tonight
+
+// function onBackKeyDown(e) {
+//     e.preventDefault();  
+//     goBack(); 
+// }
+
+// $(document).on( "pagecontainershow", function( event, ui ) {
+//     navigator.notification.alert( "The page being shown is: " + ui.toPage.prop("id") );
+// });
+
+// $(document).delegate("#login-page", "pagebeforecreate", function () {
+//     var token = localStorage.getItem(token);
+//     if(token){
+//         $.mobile.navigate("#main-page");
+//     }
+// });
+
+//=============================Log the driver out=================================================
+function logout(){
+    $("#logout").attr("onClick", "doNothing()")
+    $.mobile.loading("show");
+    localStorage.removeItem("token")
+    localStorage.removeItem("fullname")
+    localStorage.removeItem("id")
+    setTimeout(function(){
+        $.mobile.navigate("#login-page");
+    }, 2000)
+}
+
+// =============================to turn the button to unclickable================================= 
+function doNothing(){
+    return false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*// =======================Log the Driver into the app after signup from web====================================
+function login(){
     $("#login").attr("onClick", "doNothing()")
     $.mobile.loading("show");
 
@@ -129,4 +301,4 @@ function logout(){
 // =============================to turn the button to unclickable================================= 
 function doNothing(){
     return false
-}
+}*/
